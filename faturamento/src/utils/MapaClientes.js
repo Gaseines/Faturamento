a7268f5f60187ac0722c04ae0429c0e9047c9306
Gaseines <gaseinesdzn@hotmail.com>
2025-06-03 20:36:43 -0300

    Update MapaClientes.js

--- faturamento/src/utils/MapaClientes.js
+++ faturamento/src/utils/MapaClientes.js
@@ -44,7 +44,7 @@ export const clientMapping = {
   nardi: ["TRANSPORTADORA NARDI LTDA"],
   paganini: ["TRANSPORTES PAGANINI LTDA - EPP"],
   pedrao: ["PEDRAO TRANSPORTE DE CARGAS LTDA", "DJONAS CIDCLEI FERNANDES ME"],
-  picoli: ["ODAIR JOSÃ‰ PICOLI E CIA LTDA"],
+  picoli: ["ODAIR JOSE PICOLI E CIA LTDA"],
   portolog: ["PORTOLOG TRANSPORTES"],
   portoex: ["PORTOEX LOGISTICA LTDA"],
   rtm: [
